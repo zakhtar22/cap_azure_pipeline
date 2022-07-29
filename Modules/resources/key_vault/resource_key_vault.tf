6242ea581f071c54d28530fc1c9ad0e820f9bf59
usertest <zain.a.akhtar@capgemini.com>
2022-07-29 12:04:28 +0100

    access policy

--- Modules/resources/key_vault/resource_key_vault.tf
+++ Modules/resources/key_vault/resource_key_vault.tf
@@ -38,6 +38,22 @@ resource "azurerm_key_vault" "example_key_vault" {
       "Get",
     ] */
   }
+  access_policy {
+    tenant_id = data.azurerm_client_config.current.tenant_id
+    object_id = data.azurerm_client_config.current.object_id
+
+    key_permissions = [
+      "Get","Create",
+    ]
+
+    secret_permissions = [
+      "Get","Set","List","Delete","Purge","Restore",
+    ]
+
+    /* storage_permissions = [
+      "Get",
+    ] */
+  }
 }
 
 resource "azurerm_key_vault_secret" "example" {
